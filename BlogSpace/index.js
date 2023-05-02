489bb574490762fc8dd288e55f5aa8c16992c7a8
Lilian Lee <lilianlee0213@gmail.com>
2023-05-02 15:39:23 -0400

    display blogs on page

--- BlogSpace/index.js
+++ BlogSpace/index.js
@@ -1,5 +1,13 @@
-fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
-	method: 'GET',
-})
+fetch('https://apis.scrimba.com/jsonplaceholder/posts')
 	.then((response) => response.json())
-	.then((data) => console.log(data.slice(0, 5)));
+	.then((data) => {
+		const postArr = data.slice(0, 5);
+		let html = '';
+		for (let post of postArr) {
+			html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr/>`;
+		}
+		document.getElementById('post-list').innerHTML = html;
+	});
