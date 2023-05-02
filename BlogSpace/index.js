553b6a78688c5d70d193d6a8757f21a946867364
Lilian Lee <lilianlee0213@gmail.com>
2023-05-02 16:05:33 -0400

    add styling

--- BlogSpace/index.js
+++ BlogSpace/index.js
@@ -5,9 +5,12 @@ fetch('https://apis.scrimba.com/jsonplaceholder/posts')
 		let html = '';
 		for (let post of postArr) {
 			html += `
-            <h3>${post.title}</h3>
-            <p>${post.body}</p>
-            <hr/>`;
+            <div>
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+            </div>
+          
+            `;
 		}
 		document.getElementById('post-list').innerHTML = html;
 	});
