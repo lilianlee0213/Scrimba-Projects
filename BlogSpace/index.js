3d7e8568fc826e15b9ae28f002d5443f2885203d
Lilian Lee <lilianlee0213@gmail.com>
2023-05-02 16:51:53 -0400

    add new post to list of posts

--- BlogSpace/index.js
+++ BlogSpace/index.js
@@ -1,8 +1,8 @@
+let html = '';
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
 	.then((response) => response.json())
 	.then((data) => {
 		const postArr = data.slice(0, 5);
-		let html = '';
 		for (let post of postArr) {
 			html += `
             <div>
@@ -17,17 +17,26 @@ fetch('https://apis.scrimba.com/jsonplaceholder/posts')
 
 document.getElementById('new-post').addEventListener('submit', (e) => {
 	e.preventDefault();
-	const postTitle = document.getElementById('post-title').value;
-	const postBody = document.getElementById('post-body').value;
+	let postTitle = document.getElementById('post-title').value;
+	let postBody = document.getElementById('post-body').value;
 	const data = {
 		title: postTitle,
 		body: postBody,
 	};
+
 	fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
 		method: 'POST',
 		body: JSON.stringify(data),
 		headers: {'Content-Type': 'application/json'},
 	})
 		.then((res) => res.json())
-		.then((data) => console.log(data));
+		.then((post) => {
+			document.getElementById('post-list').innerHTML = `
+            <div>
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+            </div>
+            ${document.getElementById('post-list').innerHTML}
+            `;
+		});
 });
