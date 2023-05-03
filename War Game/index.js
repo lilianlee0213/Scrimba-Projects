b58b68585664ed82f705f5a31a3d3c860c088e35
Lilian Lee <lilianlee0213@gmail.com>
2023-05-03 16:16:34 -0400

    draw 2 cards from our deck

--- War Game/index.js
+++ War Game/index.js
@@ -10,3 +10,10 @@ function handleClick() {
 }
 
 document.getElementById('new-deck').addEventListener('click', handleClick);
+document.getElementById('draw-cards').addEventListener('click', () => {
+	fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+		});
+});
