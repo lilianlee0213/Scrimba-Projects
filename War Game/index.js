fc58e018e2ac049cadf36cd39855a9586ff2c084
Lilian Lee <lilianlee0213@gmail.com>
2023-05-03 17:13:26 -0400

    determine winning card

--- War Game/index.js
+++ War Game/index.js
@@ -2,7 +2,22 @@ let deckId;
 const cards = document.getElementById('cards');
 const newDeckBtn = document.getElementById('new-deck');
 const drawCardBtn = document.getElementById('draw-cards');
-
+const header = document.getElementById('header');
+const cardValues = {
+	2: 2,
+	3: 3,
+	4: 4,
+	5: 5,
+	6: 6,
+	7: 7,
+	8: 8,
+	9: 9,
+	10: 10,
+	Jack: 11,
+	Queen: 12,
+	King: 13,
+	Ace: 14,
+};
 function handleClick() {
 	fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
 		.then((res) => res.json())
@@ -15,12 +30,40 @@ drawCardBtn.addEventListener('click', () => {
 	fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
 		.then((res) => res.json())
 		.then((data) => {
-			console.log(data.cards);
 			cards.children[0].innerHTML = `
 				<img src=${data.cards[0].image} class="card" />
             `;
 			cards.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `;
+			const winnerText = determineWinner(data.cards[0], data.cards[1]);
+			header.innerText = winnerText;
 		});
 });
+
+const determineWinner = (card1, card2) => {
+	const valueOptions = [
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9',
+		'10',
+		'Jack',
+		'Queen',
+		'King',
+		'Ace',
+	];
+	const card1ValueIndex = valueOptions.indexOf(card1.value);
+	const card2ValueIndex = valueOptions.indexOf(card2.value);
+	if (card1ValueIndex > card2ValueIndex) {
+		return 'Card 1 wins!';
+	} else if (card2ValueIndex > card1ValueIndex) {
+		return 'Card 2 wins!';
+	} else {
+		return 'War!';
+	}
+};
