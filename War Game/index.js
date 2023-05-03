25c417ce90d75af062e7789dd8479f3ae6f60b85
Lilian Lee <lilianlee0213@gmail.com>
2023-05-03 16:12:56 -0400

    set up and save deckId

---
+++ War Game/index.js
@@ -0,0 +1,12 @@
+let deckId;
+
+function handleClick() {
+	fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
+		.then((res) => res.json())
+		.then((data) => {
+			deckId = data.deck_id;
+			console.log(data.deck_id);
+		});
+}
+
+document.getElementById('new-deck').addEventListener('click', handleClick);
