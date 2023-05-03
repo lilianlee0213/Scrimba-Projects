9e89e72aeb8df49da18101163d58eff0e44378e7
Lilian Lee <lilianlee0213@gmail.com>
2023-05-03 16:22:55 -0400

    display card images

--- War Game/index.js
+++ War Game/index.js
@@ -5,15 +5,22 @@ function handleClick() {
 		.then((res) => res.json())
 		.then((data) => {
 			deckId = data.deck_id;
-			console.log(data.deck_id);
 		});
 }
-
+const cards = document.getElementById('cards');
 document.getElementById('new-deck').addEventListener('click', handleClick);
 document.getElementById('draw-cards').addEventListener('click', () => {
 	fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
 		.then((res) => res.json())
 		.then((data) => {
-			console.log(data);
+			console.log(data.cards);
+			cards.innerHTML = `
+            <div class="card">
+				<img src=${data.cards[0].image} />
+			</div>
+            <div class="card">
+				<img src=${data.cards[1].image} />
+			</div>
+            `;
 		});
 });
